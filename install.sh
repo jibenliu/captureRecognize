8ae528af6e63bffd99a314e1730b61ae5cd67e86
jibenliu <jibenliu@126.com>
2020-07-30 23:02:20 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash/bin/bash
+
+
+# pip升级
+pip install --upgrade pip -i https://mirrors.aliyun.com/pypi/simple/
+
+
+# 安装包 tensorflow numpy matplotlib cv2
+
+pip install tensorflow numpy matplotlib opencv-python -i https://mirrors.aliyun.com/pypi/simple/
